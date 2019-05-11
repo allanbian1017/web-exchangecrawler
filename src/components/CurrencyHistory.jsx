a9439c0b03cd66db6e0315ac915d17e5910eab61
Tse Lun Bien <allanbian@gmail.com>
2019-05-11 08:16:09 +0000

    Adjust datepicker for mobile usage

--- src/components/CurrencyHistory.jsx
+++ src/components/CurrencyHistory.jsx
@@ -2,32 +2,63 @@ import React, { Component } from 'react';
 import {
   PropTypes, object, bool, func,
 } from 'prop-types';
-import { Container, Row, Col } from 'react-bootstrap';
+import {
+  Container, Row, Col, Button,
+} from 'react-bootstrap';
+import moment from 'moment-timezone';
 import DatePicker from 'react-datepicker';
 import CurrencyChart from './CurrencyChart';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
 class CurrencyHistory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.toggleCalendar = this.toggleCalendar.bind(this);
+  }
+
   componentWillMount() {
     const { onInit } = this.props;
     onInit();
   }
 
+  handleChange(date) {
+    const { onDateSelected } = this.props;
+    onDateSelected(date);
+    this.toggleCalendar();
+  }
+
+  toggleCalendar() {
+    const { isOpen } = this.state;
+    this.setState({ isOpen: !isOpen });
+  }
+
   render() {
     const {
-      minDate, maxDate, selDate, data, visible, onDateSelected, onLabelClick,
+      minDate, maxDate, selDate, data, visible, onLabelClick,
     } = this.props;
+    const { isOpen } = this.state;
     return (
       <Container>
         <Row>
           <Col xs={12} md={12} lg={12} style={{ textAlign: 'center' }}>
-            <DatePicker
-              minDate={minDate}
-              maxDate={maxDate}
-              selected={selDate}
-              onChange={date => onDateSelected(date)}
-            />
+            <Button variant="outline-primary" onClick={this.toggleCalendar}>
+              {moment(selDate).format('MM-DD-YYYY')}
+            </Button>
+            {isOpen && (
+              <DatePicker
+                selected={selDate}
+                onChange={this.handleChange}
+                minDate={minDate}
+                maxDate={maxDate}
+                withPortal
+                inline
+              />
+            )}
           </Col>
         </Row>
         <Row>
